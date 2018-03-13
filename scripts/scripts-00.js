e2f4fd62c2a85fa756214c35dc9ca7574cf14e23
ndevalliere <nathalie.devalliere@gmail.com>
2018-03-13 09:02:35 +0000

    new chat prototype

--- scripts/scripts-00.js
+++ scripts/scripts-00.js
@@ -2,30 +2,7 @@ function saveTextAsFile() {
      var textToWrite = document.getElementById('textArea').value;
 
      var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
-     var fileNameToSaveAs = "test.plist";
-
-     var userInput = document.createElement("p");
-     userInput.download = fileNameToSaveAs;
-    //  userInput.innerHTML = "Download File";
-     if (window.webkitURL != null)
-     {
-         // Chrome allows the link to be clicked
-         // without actually adding it to the DOM.
-         userInput.href = window.webkitURL.createObjectURL(textFileAsBlob);
-     }
-     else
-     {
-         // Firefox requires the link to be added to the DOM
-         // before it can be clicked.
-         userInput.href = window.URL.createObjectURL(textFileAsBlob);
-        //  userInput.onclick = destroyClickedElement;
-         userInput.style.display = "none";
-         document.body.appendChild(textFileAsBlob);
-     }
-
-     userInput.click();
     $(".container__grid").append(textToWrite);
-
     console.log("clicking");
  }
 
