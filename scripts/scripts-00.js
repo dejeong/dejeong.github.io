e7a69462e5e963c3ceb453aab9c1d17903bd3e70
ndevalliere <nathalie.devalliere@gmail.com>
2018-07-28 12:51:57 +0200

    prototype

--- scripts/scripts-00.js
+++ scripts/scripts-00.js
@@ -1,16 +1,80 @@
-function saveTextAsFile() {
-     var textToWrite = document.getElementById('textArea').value;
+var infoHeight = $('.site-info').innerHeight(),
+    siteInfo = $('.site-info'),
+    siteClicked = false;
 
-     var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
-    $(".container__grid").append(textToWrite);
-    console.log("clicking");
- }
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
- var button = document.getElementById('save');
- button.addEventListener('click', saveTextAsFile);
+$(window).on('load', function() {
+  siteInfo.css({'bottom': (-1)*infoHeight});
+  $('.site-projects, .site-icon, .project-preview img, .project-container').animate({
+      opacity: 1,
+    }, 1000, function() {
+      //done
+  });
+});
 
-// for(var=i; i < 1000; i++) {
-//   $.get("data"+ i +".html", function(data){
-//      $(this).children("section:first").html(data);
-//  });
-// }
+$('.site-icon').on('click', function(){
+  siteInfo.removeClass('display');
+  $('#profile').attr('src', 'assets/icon-01.png');
+  setTimeout(function () {
+    siteInfo.css({'bottom': 0});
+    siteClicked = true;
+  }, 250);
+});
+
+$('.site-info').on('click', function() {
+  if (siteClicked) {
+    siteClicked = false;
+    siteInfo.css({'bottom': (-1)*infoHeight});
+    $('#profile').attr('src', 'assets/icon-00.png');
+    setTimeout(function () {
+        siteInfo.addClass('display');
+    }, 250);
+  }
+});
+
+$('a.project-link').on('click', function(e) {
+  e.preventDefault();
+  var imgSrc = $(this).attr('id');
+      that = $(this);
+
+  if ($(this).hasClass('active')) {
+    $('.project-preview img').attr('src', ' ');
+    $(this).removeClass('active');
+    siteClicked = false;
+  }
+
+  else {
+    $('a.project-link').removeClass('active');
+    $(this).addClass('active');
+    setTimeout(function () {
+      siteClicked = true;
+    }, 250);
+
+    $('.project-preview img').attr('src', 'assets/' + imgSrc +'/' + imgSrc + '.png');
+    $('.project-preview img').attr('id', imgSrc);
+    $('.project-preview').css({
+      'left': getRandomInt(0,75) + 'vw'
+      // 'top': getRandomInt(0,15) + 'vh'
+    });
+  }
+});
+
+$('.site-close').on('click', function() {
+  if (siteClicked) {
+    $('.project-preview img').attr('src', ' ');
+    $('a.project-link').removeClass('active');
+    siteClicked = false;
+  }
+});
+
+$('.project-preview img').on('click', function() {
+  var page = $(this).attr('id');
+  $('.site-projects, .site-icon, .project-preview img').animate({
+      opacity: 0,
+    }, 750, function() {
+      location.href = 'pages/' + page + '.html';
+  });
+});
